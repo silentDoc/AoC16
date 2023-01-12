3b47b9a151bf3c1586b380648f12f86fd0083d0d
silentDoc <ramon78t@yahoo.es>
2023-01-12 12:12:28 +0100

    Day 2 - Part 2 Completed. Weird keyboard

--- AoC16/Day02/BathroomPinCode.cs
+++ AoC16/Day02/BathroomPinCode.cs
@@ -18,7 +18,7 @@ namespace AoC16.Day02
         public void ParseInput(List<string> lines)
             => lines.ForEach(x => instructions.Add(x));
 
-        Coord Move(char direction, Coord coord)
+        Coord Move(char direction, Coord coord)     // 0,0 is top left corner
             => direction switch
             {
                 'U' => coord with { y = coord.y == 0 ? 0 : coord.y - 1 },
@@ -28,26 +28,56 @@ namespace AoC16.Day02
                 _ => throw new Exception("Invalid direction")
             };
 
+        Coord Move_P2(char direction, Coord coord)  // 0,0 is center
+            => direction switch
+            {
+                'U' => coord with { y = coord.y == -2 ? -2 : Math.Max( coord.y - 1, -1 * (2-Math.Abs(coord.x)) ) },
+                'D' => coord with { y = coord.y == 2 ? 2   : Math.Min(coord.y + 1, 2 - Math.Abs(coord.x)) },
+                'L' => coord with { x = coord.x == -2 ? -2 : Math.Max(coord.x - 1, -1 * (2 - Math.Abs(coord.y))) },
+                'R' => coord with { x = coord.x == 2 ? 2   : Math.Min(coord.x + 1, 2 - Math.Abs(coord.y)) },
+                _ => throw new Exception("Invalid direction")
+            };
+
         string Digit(Coord position)
             => (position.y * 3 + position.x + 1).ToString();
 
-        (string number, Coord finalPosition) FindDigit(Coord startPosition, string instructions)
+        string Digit_P2(Coord position)
+        =>  (position.y, position.x) switch
+            {
+                (-2, 0) => "1",
+                (-1, -1) => "2",
+                (-1, 0) => "3",
+                (-1, 1) => "4",
+                (0, -2) => "5",
+                (0, -1) => "6",
+                (0, 0) => "7",
+                (0, 1) => "8",
+                (0, 2) => "9",
+                (1, -1) => "A",
+                (1, 0) => "B",
+                (1, 1) => "C",
+                (2, 0) => "D",
+                (_, _) => throw new Exception("Invalid direction"),
+            };
+
+        (string number, Coord finalPosition) FindDigit(Coord startPosition, string instructions, int part =1)
         {
             Coord current = startPosition;
             foreach (char step in instructions)
-                current = Move(step, current);
+                current = (part ==1) ? Move(step, current) : Move_P2(step, current);
 
-            return (Digit(current), current);
+            return (part ==1 ? Digit(current) : Digit_P2(current), current);
         }
 
         string FindPin(int part = 1)
         {
-            var current = new Coord() { x = 1, y = 1 };
+            var current = (part == 1) ? new Coord() { x = 1, y = 1 }    // Part 1 - Pinboard considerers 0,0 top left , start at 5 (center)
+                                      : new Coord() { x = -2, y = 0 };  // Part 2 - Pinboard considerers 0,0 the center, start at 5 (central row, leftmost)
             StringBuilder sb = new();
             
             foreach (var line in instructions)
             {
-                (string digit, current) = FindDigit(current, line);
+                (string digit, current) = FindDigit(current, line, part);
                 sb.Append(digit);
             }
             return sb.ToString();
