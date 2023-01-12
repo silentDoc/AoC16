9d5444c6dfb580df388a0cd7e95f67ca601cb4ce
silentDoc <ramon78t@yahoo.es>
2023-01-12 08:42:34 +0100

    Prepare Day 1 Main, Readme and Input

--- AoC16/Program.cs
+++ AoC16/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int day = 25;
+            int part = 1;
+            bool test = false;
+
+            string input = "./Input/day" + day.ToString();
+            input += (test) ? "_test.txt" : ".txt";
+
+            Console.WriteLine("AoC 2015 - Day {0} , Part {1} - Test Data {2}", day, part, test);
+
+            string result = day switch
+            {
+                1 => day1(input, part).ToString(),
+                _ => throw new ArgumentException("Wrong day number - unimplemented")
+            };
+            Console.WriteLine("Result : {0}", result);
+        }
+
+        static int day1(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+            return 0;
         }
     }
 }
