16a73fd3b0e43d9a22f87738f5b0b2d859481412
silentDoc <ramon78t@yahoo.es>
2023-01-14 01:42:58 +0100

    Day 9 - Part 1 - Tests in green

--- AoC16/Day09/SequenceDecompressor.cs
+++ AoC16/Day09/SequenceDecompressor.cs
@@ -10,7 +10,35 @@ namespace AoC16.Day09
     {
         public string Decompress(string input)
         {
-            throw new NotImplementedException();
+            var processText = input;
+            StringBuilder sb = new();
+
+            while(processText.Length>0)
+            {
+                var openParenthesis = processText.IndexOf("(");
+                var closeParenthesis = processText.IndexOf(")");
+
+                if (openParenthesis == -1)
+                {
+                    sb.Append(processText);
+                    break;
+                }
+
+                if (openParenthesis > 0)
+                    sb.Append(processText.Substring(0, openParenthesis));
+
+                var marker = processText.Substring(openParenthesis, closeParenthesis-openParenthesis + 1).Replace("(","").Replace(")", "");
+                var groups = marker.Split("x");
+                int count = int.Parse(groups[0]);
+                int times = int.Parse(groups[1]);
+
+                for (int i = 0; i < times; i++)
+                    sb.Append(processText.Substring(closeParenthesis + 1, count));
+
+                processText = processText.Substring(closeParenthesis + count+1);
+            }
+
+            return sb.ToString();
         }
     }
 }
