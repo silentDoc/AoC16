47fb78cfb2fc208013d6d9a14649b47e386bd06f
silentDoc <ramon78t@yahoo.es>
2023-01-15 23:58:39 +0100

    (FIX) Fixed main program typo preventing day 13 to run

--- AoC16/Program.cs
+++ AoC16/Program.cs
@@ -6,9 +6,9 @@ namespace AoC16
     {
         static void Main(string[] args)
         {
-            int day = 12;
-            int part = 2;
-            bool test = !true;
+            int day = 13;
+            int part = 1;
+            bool test = true;
 
             string input = "./Input/day" + day.ToString();
             input += (test) ? "_test.txt" : ".txt";
@@ -29,6 +29,7 @@ namespace AoC16
                 10 => day10(input, part, test).ToString(),
                 11 => day11(input, part).ToString(),
                 12 => day12(input, part).ToString(),
+                13 => day12(input, part).ToString(),
                 _ => throw new ArgumentException("Wrong day number - unimplemented")
             };
             Console.WriteLine("Result : {0}", result);
@@ -141,6 +142,12 @@ namespace AoC16
             asm.ParseInput(lines);
 
             return asm.Solve(part);
+        }
+
+        static int day13(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+            return 0;
         }
     }
 }
