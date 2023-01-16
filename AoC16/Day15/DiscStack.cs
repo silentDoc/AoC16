b7fd4fddd69b632a9e137c60b1b3f8cea4b06452
silentDoc <ramon78t@yahoo.es>
2023-01-16 23:57:56 +0100

    Day 15 - Part 1 Completed. Disc Stack

---
+++ AoC16/Day15/DiscStack.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AoC16.Day15
+{
+    class Disc
+    {
+        public int id = 0;
+        public int num_positions = 0;
+        public int startingPosition = 0;
+
+        public Disc(int id, int positions, int startingPosition)
+        {
+            this.id = id;
+            this.num_positions = positions;
+            this.startingPosition = startingPosition;
+        }
+
+        public int GetPosition(int time)
+            => (startingPosition + time) % num_positions;
+    }
+
+    internal class DiscStack
+    {
+        List<Disc> discStack = new List<Disc>();
+
+        Disc ParseLine(string line)
+        {
+            Regex inputRegex = new Regex(@"Disc #(\d) has (\d+) positions; at time=(\d), it is at position (\d+)");
+            var groups = inputRegex.Match(line).Groups;
+            return new Disc(int.Parse(groups[1].Value), int.Parse(groups[2].Value), int.Parse(groups[4].Value));
+        }
+
+        public void ParseInput(List<string> lines)
+            => lines.ForEach(line => discStack.Add(ParseLine(line)));
+
+        int FindTime(int part = 1)
+        {
+            List<int> targetPositions = new();
+            var diff = -1;
+            foreach(var disc in discStack) 
+            {
+                targetPositions.Add(module(diff, disc.num_positions));
+                diff--;
+            }
+            
+            bool found = false;
+            int time = 0;
+            while (!found)
+            { 
+                time++;
+                var currentPositions = discStack.Select(x => x.GetPosition(time)).ToList();
+                found = currentPositions.SequenceEqual(targetPositions);
+            }
+
+            return time;
+        }
+
+        int module(int number, int mod)
+            => (number > 0) ? number % mod : number + mod;
+
+        public int Solve(int part = 1)
+            => FindTime(part);
+    }
+}
--- AoC16/Program.cs
+++ AoC16/Program.cs
@@ -8,7 +8,7 @@ namespace AoC16
         {
             int day = 15;
             int part = 1;
-            bool test = !false;
+            bool test = false;
 
             string input = "./Input/day" + day.ToString();
             input += (test) ? "_test.txt" : ".txt";
@@ -165,7 +165,10 @@ namespace AoC16
         static int day15(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
-            return 0;
+            Day15.DiscStack stack = new();
+            stack.ParseInput(lines);
+
+            return stack.Solve(part);
         }
     }
 }
