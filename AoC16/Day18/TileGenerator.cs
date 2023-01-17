4b3c9c28ed81aeb98a00cc05647056d42502515f
silentDoc <ramon78t@yahoo.es>
2023-01-17 23:53:24 +0100

    Day 18  - Part 1 Completed. Generating Tiles

--- AoC16/Day18/TileGenerator.cs
+++ AoC16/Day18/TileGenerator.cs
@@ -1,15 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace AoC16.Day18
 {
     public class TileGenerator
     {
-        public string GenerateRow(string previousRow)
-            => throw new NotImplementedException();
+        string startingRow = "";
+        int rowCount = 0;
 
+        char GetPrevious(string row, int index)
+            => (index < 0) ? '.' : (index == row.Length) ? '.' : row[index];
+
+        char BuildTile((char, char, char) prevRowTriplet)
+            => prevRowTriplet switch
+            {
+                ('^', '^', '.') => '^',
+                ('.', '^', '^') => '^',
+                ('^', '.', '.') => '^',
+                ('.', '.', '^') => '^',
+                _ => '.'
+            };
+
+        public string GenerateRow(string previousRow)
+        {
+            var newRow = new StringBuilder();
+            
+            List<(char, char, char)> triplets = new List<(char, char, char)>();
+
+            for (int i = 0; i < previousRow.Length; i++)
+                triplets.Add((GetPrevious(previousRow, i - 1), GetPrevious(previousRow, i), GetPrevious(previousRow, i + 1)));
+
+            triplets.ForEach(x => newRow.Append(BuildTile(x)));
+            return newRow.ToString();
+        }
+
+        public void ParseInput(List<string> lines)
+        {
+            var groups = lines[0].Split(",", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            startingRow = groups[0];
+            rowCount = int.Parse(groups[1]);
+        }
+
+        int CountSafeTiles(int part =1)
+        {
+            var currentRow = startingRow;
+            StringBuilder sb = new();
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                sb.Append(currentRow);
+                currentRow = GenerateRow(currentRow);
+            }
+
+            return sb.ToString().Count(x => x == '.');
+        }
+
+        public int Solve(int part = 1)
+            => CountSafeTiles(part);
     }
 }
--- AoC16/Program.cs
+++ AoC16/Program.cs
@@ -8,7 +8,7 @@ namespace AoC16
         {
             int day = 18;
             int part = 1;
-            bool test = !false;
+            bool test = false;
 
             string input = "./Input/day" + day.ToString();
             input += (test) ? "_test.txt" : ".txt";
@@ -194,7 +194,10 @@ namespace AoC16
         static int day18(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
-            return 0;
+            Day18.TileGenerator tilegen = new();
+            tilegen.ParseInput(lines);
+
+            return tilegen.Solve(part);
         }
     }
 }
