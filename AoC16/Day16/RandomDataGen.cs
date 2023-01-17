918c04d4fdd93d1f18211a88c1e7b3e257cef586
silentDoc <ramon78t@yahoo.es>
2023-01-17 12:23:19 +0100

    Day 16 - Implemented core methods, tests in green

--- AoC16/Day16/RandomDataGen.cs
+++ AoC16/Day16/RandomDataGen.cs
@@ -9,10 +9,30 @@ namespace AoC16.Day16
     public class RandomDataGen
     {
         public string DragonCurve(string input)
-            => throw new NotImplementedException();
+        {
+            StringBuilder sb = new StringBuilder(input);
+            StringBuilder reversed = new();
+
+            var b = input.Reverse();
+            b = b.Select(x => (x == '0') ? '1' : '0');
+            foreach (char c in b)
+                reversed.Append(c);
+            sb.Append('0');
+            sb.Append(reversed.ToString());
+            return sb.ToString();
+        }
+
 
         public string Checksum(string input)
-            => throw new NotImplementedException();
+        {
+            StringBuilder checksum = new();
+            var groups = input.Chunk(2).Select(x => new string(x)).ToList();
+            foreach (var g in groups)
+                checksum.Append(g[0] == g[1] ? '1' : '0');
 
+            var check = checksum.ToString();
+
+            return check.Length % 2 == 0 ? Checksum(check) : check;
+        }
     }
 }
